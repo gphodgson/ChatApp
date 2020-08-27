5c3a69e70e0633cd2e48a385ccb8073f802561bb
Geoff Hodgson <geoffphodgson1@gmail.com>
2020-08-27 15:23:44 -0400

    Changed from ping api service to Client-Server pushes with Socket.io

--- chatapp-api/app.js
+++ chatapp-api/app.js
@@ -1,19 +1,27 @@
+require('dotenv').config()
 const express =  require('express');
+const app = express();
 const mysql = require('mysql');
-const https = require("https")
 const fs = require("fs");
-
+const bodyParser = require("body-parser")
 const options = {
     key: fs.readFileSync("/etc/letsencrypt/live/gh-api.com/privkey.pem"),
     cert: fs.readFileSync("/etc/letsencrypt/live/gh-api.com/fullchain.pem")
-};
+}
+
+console.log(options)
+
+const https = require("https").createServer(options, app);
+const http = require("http").createServer(app);
+const io = require('socket.io')(https)
+const ioHttp = require('socket.io')(http)
 
 // Create connection
 const db = mysql.createConnection({
-    host: 'mysql.geoffhodgson.com',
-    user: 'chatapprw',
-    password: 'S7r4wB3rr7K1w1303!',
-    database: 'chatapp_geoffhodgson'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
 });
 
 //connect
@@ -21,22 +29,53 @@ db.connect((err) => {
     if(err){
         throw err;
     }else{
-        console.log("Database connection successful.");
+        console.log('Database (' + process.env.DB_DATABASE + ') connection successful.');
     }
 });
 
-const app = express();
+io.on('connection', (socket) => {
+    console.log('User Connected...')
+    socket.on('disconnect', () => { 
+        console.log('user disconnected');
+    });
 
-app.get('/getChats', (request, response) =>{
+    socket.on('sent', () => {
+        console.log('user sent a message.');
+        io.emit('update', {});
+        
+    });
+
+    socket.on('updated', () => {
+        console.log('user updated chats...');
+    });
+});
+
+ioHttp.on('connection', (socket) => {
+    console.log('User Connected...')
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('sent', () => {
+        console.log('user sent a message.');
+        socket.broadcast.emit('update', {});
+    });
+
+    socket.on('updated', () => {
+        console.log('user updated chats...');
+    });
+});
+
+app.get('/chatapp/getChats', (request, response) =>{
     let sql = "SELECT * FROM chats";
     db.query(sql, (err, result) => {
         if(err) throw err;
-        response.set('Access-Control-Allow-Origin', 'https://www.geoffhodgson.com');
+        response.set('Access-Control-Allow-Origin', process.env.ACCESS_CONTROL);
         response.send(result);
     });
 });
 
-app.get('/addChat', (request, response) => {
+app.get('/chatapp/addChat', (request, response) => {
     let post = {text: request.query.text, sent_by: request.query.sent_by};
     let sql = "INSERT INTO chats SET ?";
     db.query(sql, post, (err, result) => {
@@ -45,15 +84,21 @@ app.get('/addChat', (request, response) => {
             text: post.text,
             sent_by: post.sent_by
         }
-	console.log("Added new chat:");
-	console.log(sendout);
-        response.set('Access-Control-Allow-Origin', 'https://www.geoffhodgson.com');
+        console.log("Added new chat:");
+        console.log(sendout);
+        response.set('Access-Control-Allow-Origin', process.env.ACCESS_CONTROL);
         response.send(sendout);
     });
+    
 });
 
-app.listen('3001', () => {
-	console.log("Listening on port 1337...")
+app.use(bodyParser.urlencoded({ extended: true }));
+
+http.listen('3001', () => {
+    console.log("Listening on 3001...")
 });
 
-https.createServer(options, app).listen(process.env.PORT || '3002');
+if(!process.env.DEV === false)
+{
+   https.listen(process.env.PORT || '443');
+}
--- chatapp-frontend/src/App.js
+++ chatapp-frontend/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import dotenv from 'dotenv';
 import Axios from 'axios';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import io from 'socket.io-client';
 
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
@@ -17,22 +18,32 @@ export class App extends Component {
     chats: []
   }
 
+  socket = io("https://gh-api.com");
+
   newChat = (text, sent_by) => 
   {
-    Axios.get('https://gh-api.com:3002/addChat?text='+text+"&sent_by="+sent_by)
+    Axios.get(process.env.REACT_APP_API_URI + 'addChat?text='+text+"&sent_by="+sent_by)
     .then(res => this.setState({chats: [...this.state.chats, res.data]}))
-    // this.setState({chats: [...this.state.chats, {id: uuid(), text, sent_by}]})
+    
+    this.socket.emit('sent');
   }
 
   updateChats = async () => {
-    Axios.get('https://gh-api.com:3002/getChats')
+    Axios.get(process.env.REACT_APP_API_URI + 'getChats')
     .then(res => this.setState({chats: res.data}));
+    this.socket.emit('updated');
   }
 
   componentDidMount() 
   {
+    dotenv.config();
+
+    // this.socket = io(process.env.REACT_APP_SOCKET);
+    this.socket.on('update', async () => {
+      this.updateChats();
+    });
+
     this.updateChats();
-    setInterval(this.updateChats, 1000)
   }
 
   render() {
@@ -40,7 +51,7 @@ export class App extends Component {
       <Router>
         <div className="App">
           <Header />
-          <Route path="/projects/chatapp" render={props => (
+          <Route path={process.env.REACT_APP_HOME_PATH} render={props => (
             <React.Fragment>
               <div style={{height: "80vh", border: "1px solid black", overflowX: "hidden", overflowY:"auto", display:"flex", flexDirection: "column-reverse"}}>
                 <div >
